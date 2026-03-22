430d20143e554717565fe855c911f9a5829a71e4
providenciadatahub-oss <providenciadatahub@gmail.com>
2026-03-22 02:30:56 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const Koop = require('@koopjs/koop')
+const osrm = require('@koopjs/provider-osrm')
+const express = require('express')
+
+const koop = new Koop()
+
+// Registramos el proveedor de rutas OSRM
+koop.register(osrm) 
+
+const app = express()
+
+// Habilitar CORS para que ArcGIS Online no bloquee la conexión
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// Koop servirá las rutas bajo el path /osrm
+app.use(koop.server)
+
+// Render detecta el puerto automáticamente
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Proxy de Rutas Providencia Activo`))
